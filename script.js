baa4d5affa4948d96452ca3dd3e44e014a263da7
MichalRais <Michal.Rais@iCloud.com>
2025-10-08 10:23:53 +0100

    task 1: Practice Basic data types

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-//Basic data types
-/*
-    number
-    string
-    boolean
+/*Task 1 - video 5
+
+Real estate agency
+
+We are describing a house. The house has 4 windows, 1 door, the color is white, the height is 50 meters, it is new, it has a garage.
+
+Your task is to enter these values ​​into the following variables and always write the correct data type for them:
+
+windows
+doors
+color
+height
+isNew (yes, no)
+garage (yes, no)
+
+write to the console:
 */
-var myName = "Mike";
-var age = 28;
-var adult = true;
-console.log(myName);
+var windows = 4;
+var doors = 1;
+var color = "white";
+var height = 50;
+var isNew = true;
+var garage = true;
+var house = {
+    windows: windows,
+    doors: doors,
+    color: color,
+    height: height,
+    isNew: isNew,
+    garage: garage
+};
+console.log(house);
--- script.ts
+++ script.ts
@@ -1,12 +1,35 @@
-//Basic data types
+/*Task 1 - video 5
 
-/*
-    number
-    string
-    boolean
-*/ 
+Real estate agency
 
-const myName: string = "Mike"
-const age: number = 28
-const adult: boolean = true
-console.log(myName);
+We are describing a house. The house has 4 windows, 1 door, the color is white, the height is 50 meters, it is new, it has a garage.
+
+Your task is to enter these values ​​into the following variables and always write the correct data type for them:
+
+windows
+doors
+color
+height
+isNew (yes, no)
+garage (yes, no)
+
+write to the console:
+*/
+
+const windows: number = 4
+const doors: number = 1
+const color: string = "white"
+const height: number = 50
+const isNew: boolean = true
+const garage: boolean = true
+
+const house = {
+    windows,
+    doors,
+    color,
+    height,
+    isNew,
+    garage
+}
+
+console.log(house)
