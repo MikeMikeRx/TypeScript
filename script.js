806602887e13ed982628b2ae65ebca015e036586
MichalRais <Michal.Rais@iCloud.com>
2025-10-08 12:21:56 +0100

    Data types: 'Any'

--- script.js
+++ script.js
@@ -1,27 +1,2 @@
-/*Real Estate Agency and House Condition
-
-You have the following object:
-
-const house = {
-color: "white"
-}
-
-Outside the object, create an enum called HouseCondition and fill it with three values:
-habitable,
-normal,
-uninhabitable.
-
-Add a property condition to the house object and assign from the enum that it is uninhabitable.
-
-Print this property to the console using console.log. Note: only the numeric value will be printed. */
-var House_Condition;
-(function (House_Condition) {
-    House_Condition[House_Condition["HABITABLE"] = 0] = "HABITABLE";
-    House_Condition[House_Condition["NORMAL"] = 1] = "NORMAL";
-    House_Condition[House_Condition["UNINHABITABLE"] = 2] = "UNINHABITABLE";
-})(House_Condition || (House_Condition = {}));
-var house = {
-    color: "white",
-    contidition: House_Condition.UNINHABITABLE
-};
-console.log(house.contidition);
+//Any
+var myArray = [true, 5, "Mike"];
--- script.ts
+++ script.ts
@@ -1,25 +1,3 @@
-/*Real Estate Agency and House Condition
+//Any
 
-You have the following object:
-
-const house = {
-color: "white"
-}
-
-Outside the object, create an enum called HouseCondition and fill it with three values:
-habitable,
-normal,
-uninhabitable.
-
-Add a property condition to the house object and assign from the enum that it is uninhabitable.
-
-Print this property to the console using console.log. Note: only the numeric value will be printed. */
-
-enum House_Condition {HABITABLE, NORMAL, UNINHABITABLE}
-
-const house = {
-    color: "white",
-    contidition: House_Condition.UNINHABITABLE
-}
-
-console.log(house.contidition);
+const myArray: any[] = [true, 5, "Mike"]
