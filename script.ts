345ac96103a06498cff4d155995138e6afc81a8a
MichalRais <Michal.Rais@iCloud.com>
2025-10-08 10:45:21 +0100

    Task 2: Practice Objects

--- script.ts
+++ script.ts
@@ -1,15 +1,30 @@
-// Basic data types: Objects
+// const windows = 4
+// const doors = 1
+// const color = "white"
+// const height = 50
+// const isNew = true
+// const garage = true
 
-const person: {
-    name: string,
-    age: number,
-    adult: boolean
-} = {
-    name: "Michal",
-    age: 28,
-    adult: true
+
+/*
+Real estate office object
+Create an object called house that will contain all the variables listed above. Use the simplest possible notation (you don't need to specify which variable is which type)
+
+List all properties to the console
+*/
+
+const house = {
+    windows: 4,
+    doors: 1,
+    color: "white",
+    height: 50,
+    isNew: true,
+    garage: true
 }
 
-console.log(person.name);
-console.log(person.age);
-console.log(person.adult);
+console.log(house.windows);
+console.log(house.doors);
+console.log(house.color);
+console.log(house.height);
+console.log(house.isNew);
+console.log(house.garage);
