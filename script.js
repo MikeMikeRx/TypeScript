5c604080ab8aee5e6240ac12784ef7beae033851
MichalRais <Michal.Rais@iCloud.com>
2025-10-08 10:34:10 +0100

    Basic data types: Type inference

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-/*Task 1 - video 5
-
-Real estate agency
-
-We are describing a house. The house has 4 windows, 1 door, the color is white, the height is 50 meters, it is new, it has a garage.
-
-Your task is to enter these values ​​into the following variables and always write the correct data type for them:
-
-windows
-doors
-color
-height
-isNew (yes, no)
-garage (yes, no)
-
-write to the console:
-*/
+// Type inference
+/*Improved Real estate agency*/
 var windows = 4;
 var doors = 1;
 var color = "white";
--- script.ts
+++ script.ts
@@ -1,27 +1,13 @@
-/*Task 1 - video 5
+// Type inference
 
-Real estate agency
+/*Improved Real estate agency*/
 
-We are describing a house. The house has 4 windows, 1 door, the color is white, the height is 50 meters, it is new, it has a garage.
-
-Your task is to enter these values ​​into the following variables and always write the correct data type for them:
-
-windows
-doors
-color
-height
-isNew (yes, no)
-garage (yes, no)
-
-write to the console:
-*/
-
-const windows: number = 4
-const doors: number = 1
-const color: string = "white"
-const height: number = 50
-const isNew: boolean = true
-const garage: boolean = true
+const windows = 4
+const doors = 1
+const color = "white"
+const height = 50
+const isNew = true
+const garage = true
 
 const house = {
     windows,
@@ -32,4 +18,4 @@ const house = {
     garage
 }
 
-console.log(house)
+console.log(house);
