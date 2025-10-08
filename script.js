97650a32ee0194b6992996f6fad522b124191eaa
MichalRais <Michal.Rais@iCloud.com>
2025-10-08 12:37:24 +0100

    Data types: 'Union type'

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//Any
-var myArray = [true, 5, "Mike"];
+// Union type
+function combinationX(input1, input2) {
+    var result;
+    if (typeof (input1) === "number" && typeof (input2) === "number") {
+        result = input1 + input2;
+        return result;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+        return result;
+    }
+}
+console.log(combinationX(5, 6));
+console.log(combinationX("Michal", "Rais"));
--- script.ts
+++ script.ts
@@ -1,3 +1,16 @@
-//Any
+// Union type
 
-const myArray: any[] = [true, 5, "Mike"]
+function combinationX (input1: string | number, input2: string | number){
+    let result: number | string
+    if(typeof(input1) === "number" && typeof(input2) === "number"){
+        result = input1 + input2
+        return result
+    } else {
+        result = input1.toString() + input2.toString()
+        return result
+    }
+}
+
+console.log(combinationX(5,6));
+console.log(combinationX("Michal", "Rais"));
+
