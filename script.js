6c5a0f4760385e5e34a24a0e32a574df975469bc
MichalRais <Michal.Rais@iCloud.com>
2025-10-09 09:54:34 +0100

    TypeScript: 'Type alias / custom type'

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Literal type
 function combination(input1, input2, resultType) {
     var result;
     if (typeof (input1) === "number" && typeof (input2) === "number" || resultType === "as-number") {
@@ -9,14 +8,8 @@ function combination(input1, input2, resultType) {
         result = input1.toString() + input2.toString();
         return result;
     }
-    // if(resultType === "as-number"){
-    //     result = +result
-    //     return result
-    // } else {
-    //     return result.toLocaleString()
-    // }   
 }
-console.log(combination(5, 6, "as-number"));
-console.log(combination("Michal", "Rais", "as-text"));
-console.log(combination("10", "8", "as-number"));
-console.log(combination("10", 3, "as-number"));
+console.log(combination(4, 8, "as-number"));
+console.log(combination("Rais", "Michal", "as-text"));
+console.log(combination("10", "25", "as-number"));
+console.log(combination("10", 5, "as-number"));
--- script.ts
+++ script.ts
@@ -1,24 +1,19 @@
-// Literal type
+// Type alias / custom type
+type Combination = string | number
+type resultType = "as-number" | "as-text"
 
-function combination (input1: string | number, input2: string | number, resultType: "as-number" | "as-text"){
-    let result: number | string
+function combination (input1: Combination, input2: Combination, resultType: resultType){
+    let result: Combination
     if(typeof(input1) === "number" && typeof(input2) === "number" || resultType === "as-number"){
         result = +input1 + +input2
         return result
     } else {
         result = input1.toString() + input2.toString()
         return result
-    }
-
-    // if(resultType === "as-number"){
-    //     result = +result
-    //     return result
-    // } else {
-    //     return result.toLocaleString()
-    // }   
+    } 
 }
 
-console.log(combination(5, 6, "as-number"));
-console.log(combination("Michal", "Rais", "as-text"));
-console.log(combination("10", "8", "as-number"))
-console.log(combination("10", 3, "as-number"));
+console.log(combination(4, 8, "as-number"));
+console.log(combination("Rais", "Michal", "as-text"));
+console.log(combination("10", "25", "as-number"))
+console.log(combination("10", 5, "as-number"));
