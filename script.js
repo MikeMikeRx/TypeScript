2e2046377ad5b230511ef595f998e8a300853957
MichalRais <Michal.Rais@iCloud.com>
2025-10-09 10:38:31 +0100

    Task 6: Practice functions

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-// Return and void
-function sum(n1, n2) {
-    return n1 + n2;
+/*Consider the following object that we created earlier:
+
+const house = {
+    windows: 4,
+    doors: 1,
+    color: "white",
+    height: 50
 }
-var result = sum(5, 4);
-function sum2(n1, n2) {
-    return n1.toString() + n2.toString();
+
+Your task is to create a function that accepts this object as its only argument and prints the following sentence to the console:
+
+"This house has 4 windows, 1 door, its color is white, and its height is 50 meters."
+
+The function should accept a single parameter (which you can name as you like). When calling the function, simply pass the house object.
+
+You may use either string concatenation (+) or template literals (backticks ` ` with ${expression}) to construct the output sentence. The choice is yours.*/
+var house = {
+    windows: 4,
+    doors: 1,
+    color: "white",
+    height: 50
+};
+var house2 = {
+    windows: 8,
+    doors: 1,
+    color: "blue",
+    height: 70
+};
+function description(x) {
+    console.log("This house has ".concat(x.windows, " windows, ").concat(x.doors, " door, its color is ").concat(x.color, " and its height is ").concat(x.height, " meters"));
 }
-//void
-function writeResult(num) {
-    console.log("Result: ".concat(num));
-}
-writeResult(15);
+description(house2);
--- script.ts
+++ script.ts
@@ -1,18 +1,37 @@
-// Return and void
+/*Consider the following object that we created earlier:
 
-function sum(n1:number, n2:number): number {
-    return n1 + n2
+const house = {
+    windows: 4,
+    doors: 1,
+    color: "white",
+    height: 50
 }
 
-const result = sum(5,4)
+Your task is to create a function that accepts this object as its only argument and prints the following sentence to the console:
 
-function sum2(n1: number, n2: number): string{
-    return n1.toString() + n2.toString()
+"This house has 4 windows, 1 door, its color is white, and its height is 50 meters."
+
+The function should accept a single parameter (which you can name as you like). When calling the function, simply pass the house object.
+
+You may use either string concatenation (+) or template literals (backticks ` ` with ${expression}) to construct the output sentence. The choice is yours.*/
+
+const house = {
+    windows: 4,
+    doors: 1,
+    color: "white",
+    height: 50
 }
 
-//void
-function writeResult(num: number){
-    console.log(`Result: ${num}`)
+const house2 = {
+    windows: 8,
+    doors: 1,
+    color: "blue",
+    height: 70
 }
 
-writeResult(15)
+
+function description (x){
+    console.log(`This house has ${x.windows} windows, ${x.doors} door, its color is ${x.color} and its height is ${x.height} meters`);   
+}
+
+description(house2)
