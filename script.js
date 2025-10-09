ad18f9fe9c1fce09972e60b1bdebbca56e4d4a91
MichalRais <Michal.Rais@iCloud.com>
2025-10-09 10:48:07 +0100

    TypeScript: 'Function as a Type'

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-/*Consider the following object that we created earlier:
-
-const house = {
-    windows: 4,
-    doors: 1,
-    color: "white",
-    height: 50
+// Function as a Type
+function sum(num1, num2) {
+    return num1 + num2;
 }
-
-Your task is to create a function that accepts this object as its only argument and prints the following sentence to the console:
-
-"This house has 4 windows, 1 door, its color is white, and its height is 50 meters."
-
-The function should accept a single parameter (which you can name as you like). When calling the function, simply pass the house object.
-
-You may use either string concatenation (+) or template literals (backticks ` ` with ${expression}) to construct the output sentence. The choice is yours.*/
-var house = {
-    windows: 4,
-    doors: 1,
-    color: "white",
-    height: 50
-};
-var house2 = {
-    windows: 8,
-    doors: 1,
-    color: "blue",
-    height: 70
-};
-function description(x) {
-    console.log("This house has ".concat(x.windows, " windows, ").concat(x.doors, " door, its color is ").concat(x.color, " and its height is ").concat(x.height, " meters"));
+function test(description) {
+    return description;
 }
-description(house2);
+var myNumber = 5;
+var myString = "Michal";
+// let myFunction: Function
+var myFunction;
+myFunction = sum;
+// myFunction = test
+console.log(myFunction(15, 25));
--- script.ts
+++ script.ts
@@ -1,37 +1,19 @@
-/*Consider the following object that we created earlier:
+// Function as a Type
 
-const house = {
-    windows: 4,
-    doors: 1,
-    color: "white",
-    height: 50
+function sum(num1: number, num2: number){
+    return num1 + num2
 }
 
-Your task is to create a function that accepts this object as its only argument and prints the following sentence to the console:
-
-"This house has 4 windows, 1 door, its color is white, and its height is 50 meters."
-
-The function should accept a single parameter (which you can name as you like). When calling the function, simply pass the house object.
-
-You may use either string concatenation (+) or template literals (backticks ` ` with ${expression}) to construct the output sentence. The choice is yours.*/
-
-const house = {
-    windows: 4,
-    doors: 1,
-    color: "white",
-    height: 50
+function test(description: string){
+    return description   
 }
 
-const house2 = {
-    windows: 8,
-    doors: 1,
-    color: "blue",
-    height: 70
-}
+let myNumber = 5
+let myString = "Michal"
 
+// let myFunction: Function
+let myFunction: (x: number, y: number) => number
+myFunction = sum
+// myFunction = test
 
-function description (x){
-    console.log(`This house has ${x.windows} windows, ${x.doors} door, its color is ${x.color} and its height is ${x.height} meters`);   
-}
-
-description(house2)
+console.log(myFunction(15,25))
